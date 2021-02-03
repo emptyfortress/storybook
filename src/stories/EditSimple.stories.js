6d09baa10d97b0f2548dd5ebb53346add0e098ea
Volkov Alexander <volkov.525@gmail.com>
2021-02-03 11:34:51 +0300

    simple inline edit works

--- src/stories/EditSimple.stories.js
+++ src/stories/EditSimple.stories.js
@@ -1,6 +1,26 @@
 import EditSimple from './EditSimple'
 
 export default {
-	title: 'Edit/Simple',
-	component: EditSimple
+	title: 'Edit in place/Simple',
+	component: EditSimple,
+	args: {
+		dark: false,
+	},
+	argTypes: {
+		locale: {
+			defaultValue: 'en',
+			control: {
+				type: 'inline-radio',
+				options: { English: 'en', Русский: 'ru' },
+			},
+		},
+	},
 }
+
+const Template = (args, { argTypes }) => ({
+	props: Object.keys(argTypes),
+	components: { EditSimple },
+	template: '<edit-simple />',
+})
+
+export const Simple = Template.bind({})
--- src/stories/EditSimple.vue
+++ src/stories/EditSimple.vue
@@ -1,18 +1,57 @@
 <template lang="pug">
-h2 Hello!
+.line
+	.content(tabindex="0" contenteditable id="div" ref="div") {{ $t('clicktoedit') }}
+	v-icon(small @click="clear").clear mdi-close-circle
 </template>
 
 <script>
 
 export default {
-	data() {
-		return {
+	methods: {
+		clear () {
+			this.$refs.div.textContent = ''
+			this.$refs.div.focus()
 		}
-	},
+	}
 }
-
 </script>
 
 <style scoped lang="scss">
+@import '@/assets/css/colors.scss';
 
+.line {
+	line-height: 130%;
+	color: $link;
+	display: inline-block;
+	padding: 0 1.6rem 0 0;
+	position: relative;
+	&:focus-within {
+		border-bottom: 1px dotted $link;
+		.clear {
+			display: block;
+		}
+	}
+}
+.content {
+	min-width: 50px;
+	&:focus {
+		outline: none;
+		color: #000;
+	}
+}
+.clear {
+	display: none;
+	position: absolute;
+	right: 0;
+	top: 50%;
+	transform: translateY(-50%);
+	cursor: pointer;
+	color: #aaa;
+	&:hover {
+		color: #333;
+	}
+}
+.theme--dark .content:focus {
+	color: #ccc;
+}
 </style>
